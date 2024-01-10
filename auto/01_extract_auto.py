bae311f5c3aa777d78f71a3f573a9b298b8e840f
Amsyar Malek <amsyar1989@gmail.com>
2024-01-10 15:54:26 +0800

    Add some changes

--- auto/01_extract_auto.py
+++ auto/01_extract_auto.py
@@ -48,8 +48,8 @@ def job():
 
 # Define schedules for the job
 # schedule.every(5).minutes.do(job)  # Every 5 minutes
-schedule.every(60).minutes.do(job)  # Every 60 minutes
-# schedule.every(480).minutes.do(job)  # Every 60 minutes
+# schedule.every(60).minutes.do(job)  # Every 60 minutes
+schedule.every(720).minutes.do(job)  # Every 12 hours
 # schedule.every(8).hours.do(job)  # Every 60 minutes
 # schedule.every(8).hours.at("15:40").do(job)  # Every 12 hours starting at 5 AM
 # schedule.every().day.at("06:00").do(job)  # Every day at 6 AM
