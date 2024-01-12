481b5aea41d619dabb75e1bb2527e3d13a785b9e
Amsyar Malek <amsyar1989@gmail.com>
2024-01-13 01:04:05 +0800

    changes

--- auto/auto_run.sh
+++ auto/auto_run.sh
@@ -2,11 +2,11 @@
 cd /root/projects/PropertyScraper/prj_venv
 source bin/activate
 cd /root/projects/PropertyScraper/src/01_extract_linux-64
-python3 extract.py
-cd -
+python3 extract.py && \
+cd - && \
 cd /root/projects/PropertyScraper/src/02_transform
-python3 transform.py
-cd -
+python3 transform.py && \
+cd - && \
 cd /root/projects/PropertyScraper/src/03_load
 python3 staging_pgsql_iproperty.py
 cd -
