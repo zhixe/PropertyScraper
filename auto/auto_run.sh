98657de18dd0a6dbdfe5b024a2233a2ba731ba51
Amsyar Malek <amsyar1989@gmail.com>
2024-01-15 09:26:29 +0800

    Add cleaning

--- auto/auto_run.sh
+++ auto/auto_run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+set -e
+
 cd /root/projects/PropertyScraper/prj_venv
 source bin/activate
 cd /root/projects/PropertyScraper/src/01_extract_linux-64
-python3 extract.py && \
-cd - && \
-cd /root/projects/PropertyScraper/src/02_transform
-python3 transform.py && \
-cd - && \
-cd /root/projects/PropertyScraper/src/03_load
+python3 extract.py
+
+cd - && cd /root/projects/PropertyScraper/src/02_transform
+python3 transform.py
+
+cd - && cd /root/projects/PropertyScraper/src/03_load
 python3 staging_pgsql_iproperty.py
+
 cd -
 deactivate
+trap 'echo "An error occurred. Exiting the program.."; exit 1' ERR
--- auto/cleaning.sh
+++ auto/cleaning.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Directory where the .csv files are located
 CSV="/root/projects/PropertyScraper/data/raw"
@@ -7,3 +8,4 @@ LOG="/root/projects/PropertyScraper/logs"
 # Find and delete .csv files older than 2 days
 find "$CSV" -name '*.csv' -mtime +1 -exec rm {} \;
 find "$LOG" -name '*.log' -mtime +1 -exec rm {} \;
+trap 'echo "An error occurred. Exiting the program.."; exit 1' ERR
